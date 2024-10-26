32ce61520c5f4c7c00cd6bae27eb99bf942de062
Simon <simon.williams1985@outlook.com>
2024-10-26 20:48:10 +0100

    7th change

--- src/App.jsx
+++ src/App.jsx
@@ -9,13 +9,13 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    
       <div>
         <Navbar />
         <Envelope />
       </div>
      
-    </>
+   
   )
 }
 
--- src/Conponents/Navbar/Navbar.jsx
+++ src/Conponents/Navbar/Navbar.jsx
@@ -4,7 +4,7 @@ import './Navbar.css'
 const Navbar = () => {
   return (
     <div>
-        hello
+        hello you
     </div>
   )
 }
